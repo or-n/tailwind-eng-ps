86e8d98aa891154c6d8d85b76b730941f261096c
bao <baomai@berkeley.edu>
2024-01-16 21:47:31 -0800

    notion link

--- src/app/api/price/route.ts
+++ src/app/api/price/route.ts
@@ -5,7 +5,8 @@ export async function GET(req: any) {
   const { searchParams } = new URL(req.url);
   const ids = searchParams.get('ids')?.split(',')
     ?? [];
-  // ignore this
+  // ignore this, it's not the bug
+  // we just want to work with fake data prices 
   const prices = ids.map((id) => id.length * 1_000)
 
   return NextResponse.json({
