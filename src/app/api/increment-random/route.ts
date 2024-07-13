3a9d4ae9d00cc29ec288f336900ed1da855be6e8
or-n <kulombowie@gmail.com>
2024-07-13 16:53:46 +0200

    fix ps6 (fib)

--- src/app/api/increment-random/route.ts
+++ src/app/api/increment-random/route.ts
@@ -6,7 +6,7 @@ let history: number[] = []
 
 export async function POST(req: any) {
     const increment = Math.floor(Math.random() * 10) + 1
-    const previous = history.length > 2
+    const previous = history.length >= 2
         ? history[history.length - 1] + history[history.length - 2]
         : 0
     history.push(counter + previous)
